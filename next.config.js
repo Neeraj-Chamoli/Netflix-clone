544c47775f1e1d3f411576c8501c0e29f9acf2a2
Neeraj-Chamoli-Pragmaapps <127183462+Neeraj-Chamoli-Pragmaapps@users.noreply.github.com>
2023-06-16 17:45:03 +0530

    Configure Next.js for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/Netflix-clone',
+  assetPrefix: '/Netflix-clone/',
 }
 
 module.exports = nextConfig
